e1c8a0b9a0f0341acc95fe83c473c20a8996ce5c
infinitepush <piyush89101@gmail.com>
2025-06-30 23:40:40 +0530

    L6 codes

---
+++ L6 (DOM)/index1.js
@@ -0,0 +1,16 @@
+console.log("we should know how to connect html, css and js ");
+console.log("html ko css se link karne ke liye:: ");
+console.log("(<link rel = stylesheet, href = nameofcssfile.css)");
+console.log("to link html with the css file");
+console.log(" inside the end of the body tag <script src = jsfilename.js> </script>");
+console.log("we can look at the html codes of any file in the elements tab of window ");
+console.log("we can also look at the css beneath the elements tab of the same window");
+console.log("we can link multiple css and js file to a single html file as when we deal with big projects");
+console.log("WE WILL DISCUSS WINDOW OBJECT NOW::");
+console.log("The window object represents an open window in a browser.");
+console.log("It is browser's object (not js) and is automatically created by browser");
+console.log("it is global object with lots of properties & methods.");
+console,log("console.log is also a part of window object, alert,prompt method is also part of window object");
+console.log("We will discuss DOM now::");
+
+
